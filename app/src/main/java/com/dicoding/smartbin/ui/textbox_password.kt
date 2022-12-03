a1a8187640a7300291f394603a976d0d5c0f891d
Yanwar Seto <yanwarsetop@gmail.com>
2022-12-03 23:53:59 +0700

    Login + Password Layout
    - adding layout login and password

--- app/src/main/java/com/dicoding/smartbin/ui/textbox_password.kt
+++ app/src/main/java/com/dicoding/smartbin/ui/textbox_password.kt
@@ -6,31 +6,32 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.util.AttributeSet
 import android.view.View
-import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.content.ContextCompat
 import com.dicoding.smartbin.R
 
-class textbox:AppCompatEditText {
+class textbox_password:AppCompatEditText {
+
     constructor(context: Context) : super(context) {
         init()
     }
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
         init()
     }
+
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
         init()
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        background = ContextCompat.getDrawable(context, R.drawable.edittextview)
-        hint = context.getString(R.string.wrongname)
+        background = ContextCompat.getDrawable(context, R.drawable.textbox_view)
+        hint = context.getString(R.string.password)
         textAlignment = View.TEXT_ALIGNMENT_VIEW_START
     }
 
     private fun init() {
-        addTextChangedListener(object : TextWatcher {
+        addTextChangedListener(object: TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
             }
@@ -41,10 +42,15 @@ class textbox:AppCompatEditText {
 
             override fun afterTextChanged(p0: Editable) {
                 if (p0.toString().isEmpty()){
-                    error = "Masukkan terlebih dahulu nama"
+                    error = "Masukkan terlebih dahulu password"
                 }
-
+                if (p0.toString().length < 6) {
+                    error = context.getString(R.string.passwordsalah)
+                }
             }
+
         })
     }
+
+
 }

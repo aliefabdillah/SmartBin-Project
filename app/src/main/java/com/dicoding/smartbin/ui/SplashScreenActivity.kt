2a6649bec09d6d5f807fac485667eee48394f815
aliefabdillah <aliefmabdillah09@gmail.com>
2022-12-06 20:05:18 +0700

    Commit - modified code from dev_ui_yanwar

--- app/src/main/java/com/dicoding/smartbin/ui/SplashScreenActivity.kt
+++ app/src/main/java/com/dicoding/smartbin/ui/SplashScreenActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import com.dicoding.smartbin.R
 import com.dicoding.smartbin.ui.login.LoginActivity
 
@@ -12,7 +13,7 @@ class SplashScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()

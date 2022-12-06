21e0162708a7ae0d9e90836a4696e16e46d548d8
aliefabdillah <aliefmabdillah09@gmail.com>
2022-12-06 19:56:03 +0700

    Commit - merge dev_ui_yanwar to dev

---
+++ app/src/main/java/com/dicoding/smartbin/ui/SplashScreenActivity.kt
@@ -0,0 +1,21 @@
+package com.dicoding.smartbin.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.dicoding.smartbin.R
+import com.dicoding.smartbin.ui.login.LoginActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        },4000)
+    }
+}
---
+++ app/src/main/java/com/dicoding/smartbin/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.smartbin.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.smartbin.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}

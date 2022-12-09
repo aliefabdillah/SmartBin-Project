c96e1284bb6096bff5bb280a7b3d6b95f36b205e
aliefabdillah <aliefmabdillah09@gmail.com>
2022-12-09 21:01:30 +0700

    Commit - Set Button Timer clicked once per day

--- app/src/main/java/com/dicoding/smartbin/ui/message/MessageFragment.kt
+++ app/src/main/java/com/dicoding/smartbin/ui/message/MessageFragment.kt
@@ -1,36 +1,60 @@
 package com.dicoding.smartbin.ui.message
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.net.Uri
-import android.net.wifi.rtt.CivicLocationKeys
-import android.net.wifi.rtt.CivicLocationKeys.STATE
 import android.os.Bundle
-import android.os.CountDownTimer
-import android.os.Handler
-import android.os.Looper
-import android.provider.ContactsContract
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.core.content.ContextCompat
-import androidx.core.content.ContextCompat.startActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.dicoding.smartbin.R
+import com.dicoding.smartbin.databinding.ActivityHomeBinding
 import com.dicoding.smartbin.databinding.FragmentMessageBinding
 import com.dicoding.smartbin.modelsfactory.ViewModelFactory
+import com.dicoding.smartbin.ui.home.HomeFragment
 import java.net.URLEncoder
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class MessageFragment : Fragment() {
 
     private var _binding: FragmentMessageBinding? = null
     private val binding get() = _binding!!
+    private lateinit var homeActivityBinding: ActivityHomeBinding
 
     private val messageViewModel: MessageViewModel by viewModels { ViewModelFactory.getInstance(requireActivity()) }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?, ): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         _binding = FragmentMessageBinding.inflate(inflater,container, false)
+        homeActivityBinding = ActivityHomeBinding.inflate(layoutInflater)
+
+        val sp = context!!.getSharedPreferences("SmartBin", Context.MODE_PRIVATE)
+        val savedDateTime = sp.getString("CLICK_DATE", "")
+        if ("" == savedDateTime) {
+            binding.btnRequest.isEnabled = true
+            binding.btnRequest.setBackgroundColor(ContextCompat.getColor(requireActivity(), R.color.light_green))
+        } else {
+            val dateStringNow = SimpleDateFormat("MM/dd/yyyy", Locale.getDefault()).format(Calendar.getInstance().time)
+            //compare savedDateTime with today's datetime (dateStringNow), and act accordingly
+            if (savedDateTime == dateStringNow){
+                binding.btnRequest.isEnabled = false
+                binding.btnRequest.setBackgroundColor(ContextCompat.getColor(requireActivity(), R.color.grey))
+            }else{
+                binding.btnRequest.isEnabled = true
+                binding.btnRequest.setBackgroundColor(ContextCompat.getColor(requireActivity(), R.color.light_green))
+            }
+
+        }
 
         setupView()
 
@@ -39,9 +63,26 @@ class MessageFragment : Fragment() {
 
     private fun setupView() {
         binding.btnRequest.setOnClickListener {
+            val dateString: String =
+                SimpleDateFormat("MM/dd/yyyy", Locale.getDefault()).format(Calendar.getInstance().time)
+            val sp = context!!.getSharedPreferences("SmartBin", Context.MODE_PRIVATE)
+            val editor: SharedPreferences.Editor = sp.edit()
+            editor.putString("CLICK_DATE", dateString)
+            editor.commit()
+
             val message = binding.messageBox.text.toString()
-            //            composeEmail(message)
-            composeWA(message)
+            composeEmail(message)
+
+            //set fragment to homeactivity
+            val fragment = HomeFragment()
+            val fragmentManager = activity?.supportFragmentManager
+            val fragmentTransaction = fragmentManager?.beginTransaction()
+            fragmentTransaction?.replace(R.id.nav_host_fragment_activity_home,fragment)
+            fragmentTransaction?.commit()
+
+            val navView = findNavController()
+            navView.navigate(R.id.navigation_home)
+
         }
     }
 
@@ -84,7 +125,6 @@ class MessageFragment : Fragment() {
     }
 
     companion object {
-        private var STATE = false
         private const val Phone = "6282215760138"
         private const val ADDRESS = "aliefmabdillah09@gmail.com"
         private const val SUBJECT = "REQUEST JEMPUT"
